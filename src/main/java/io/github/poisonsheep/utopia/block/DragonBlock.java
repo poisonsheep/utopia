b50aaacd02278777517e73ffa18ec672e3c1d550
butday <798971796@qq.com>
2022-10-12 03:05:18 +0800

    5

--- src/main/java/io/github/poisonsheep/utopia/block/DragonBlock.java
+++ src/main/java/io/github/poisonsheep/utopia/block/DragonBlock.java
@@ -4,11 +4,20 @@ package io.github.poisonsheep.utopia.block;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.Material;
 
 public class DragonBlock extends Block {
-    public DragonBlock(){
-        super(BlockBehaviour.Properties.of(Material.STONE).dynamicShape().sound(SoundType.WET_GRASS).noCollission().requiresCorrectToolForDrops().strength(0.2F).friction(0.9F));
-    }
+    private static IntegerProperty STATE = IntegerProperty.create("dragon", 0, 1);
 
+    public DragonBlock() {
+        super(BlockBehaviour.Properties.of(Material.STONE).dynamicShape().sound(SoundType.WET_GRASS).requiresCorrectToolForDrops().strength(0.2F).friction(0.9F));
+        this.registerDefaultState(this.defaultBlockState().setValue(STATE, 1));
+    }
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState>builder){
+        builder.add(STATE);
+    }
 }
